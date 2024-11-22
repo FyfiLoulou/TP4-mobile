4a77dd0b8448df447d672a14345152ec57d4d765
Félix Blanchette <louirlesbiron6@gmail.com>
2024-11-22 11:22:44 -0500

    (fix #14)

--- app/src/main/java/ecole/naji/tp4/UpdateActivite.java
+++ app/src/main/java/ecole/naji/tp4/UpdateActivite.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,29 +12,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class incriptiionActivyt extends AppCompatActivity {
+public class UpdateActivite extends AppCompatActivity {
     private EditText emailInput;
     private EditText pwInput;
     private EditText nomInpout;
     private EditText adressInput;
     private EditText telInpout;
-    private Button buttonCrrer;
+    private Button buttonUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inscription);
+        setContentView(R.layout.update_dude);
         initEditField();
     }
 
     private void initEditField() {
 
-        nomInpout = findViewById(R.id.nom);
-        adressInput = findViewById(R.id.adresse);
-        telInpout = findViewById(R.id.tel);
-        emailInput = findViewById(R.id.email);
-        pwInput = findViewById(R.id.pw);
-        buttonCrrer = findViewById(R.id.creer);
+        nomInpout = findViewById(R.id.nom2);
+        adressInput = findViewById(R.id.adresse2);
+        telInpout = findViewById(R.id.tel2);
+        emailInput = findViewById(R.id.email2);
+        pwInput = findViewById(R.id.pw2);
+        buttonUpdate = findViewById(R.id.updateButton);
 
         emailInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -44,7 +43,7 @@ public class incriptiionActivyt extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (!isValidate(emailInput.getText().toString())) {
                     emailInput.setError("Addresse Courriel non valide");
-                    buttonCrrer.setEnabled(false);
+                    buttonUpdate.setEnabled(false);
                 }}
             @Override
             public void afterTextChanged(Editable s) {}
@@ -57,7 +56,7 @@ public class incriptiionActivyt extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (nomInpout.getText().toString().length() > 2) {
                     nomInpout.setError("Nom non pas non valide");
-                    buttonCrrer.setEnabled(false);
+                    buttonUpdate.setEnabled(false);
                 }}
             @Override
             public void afterTextChanged(Editable s) {}
@@ -70,7 +69,7 @@ public class incriptiionActivyt extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (pwInput.getText().toString().length() < 3) {
                     pwInput.setError("Pas bon mot passe");
-                    buttonCrrer.setEnabled(false);
+                    buttonUpdate.setEnabled(false);
                 }}
             @Override
             public void afterTextChanged(Editable s) {}
@@ -83,18 +82,18 @@ public class incriptiionActivyt extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (nomInpout.getText().toString().length() < 9) {
                     nomInpout.setError("Télephone non valide");
-                    buttonCrrer.setEnabled(false);
+                    buttonUpdate.setEnabled(false);
                 }}
             @Override
             public void afterTextChanged(Editable s) {}
         });
 
 
-        buttonCrrer.setOnClickListener(e->{
+        buttonUpdate.setOnClickListener(e->{
             String nom = nomInpout.getText().toString(), email = emailInput.getText().toString(), pw = pwInput.getText().toString(), adresse = adressInput.getText().toString(), tel = telInpout.getText().toString();
 
             startActivity(new Intent(this, connexionActivuty.class));
-            // TODO STORE BD
+            // TODO UPDATE STORE BD
         });
 
     }
