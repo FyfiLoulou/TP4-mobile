84a0c9e1f0222a93464689d89fc1830da078bdb5
Louis-Charles Biron et Félix Blanchette <felix.blanchette@gmail.com>
2024-11-22 10:20:51 -0500

    trust sa marche

--- app/src/main/java/ecole/naji/tp4/MainActivity.java
+++ app/src/main/java/ecole/naji/tp4/MainActivity.java
@@ -1,24 +1,65 @@
 package ecole.naji.tp4;
 
 import android.os.Bundle;
+import android.view.Gravity;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout dLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.accueil);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(v -> dLayout.openDrawer(Gravity.LEFT));
+        setNavigationDrawer();
+    }
+    private void setNavigationDrawer() {
+        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(item -> {
+            navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    Fragment fragment = null;
+                    int itemId = menuItem.getItemId();
+/*
+                    switch (itemId) {
+                        case R.id.accueil:
+                            fragment = new FragmentAccueil();
+                            break;
+                    }
+*/
+                    Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                    if (fragment != null) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame, fragment);
+                        transaction.commit();
+                        dLayout.closeDrawers();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            return false;
         });
     }
 }
