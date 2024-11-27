395b0df1412c711f987babc96a9a4b761ab5da35
Louis-Charles Biron et Félix Blanchette <felix.blanchette@gmail.com>
2024-11-27 13:53:29 -0500

    QUE J'EN VOIT UN QUI TOUCHE A FUCKING MAIN ACTIVITY JE LE DÉCOLISS SOLIDE SA MERE

--- app/src/main/java/ecole/naji/tp4/MainActivity.java
+++ app/src/main/java/ecole/naji/tp4/MainActivity.java
@@ -1,17 +1,14 @@
 package ecole.naji.tp4;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -24,20 +21,24 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout dLayout;
-    Button conn;
-    Button signup;
+    boolean userConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.accueil);
+        setContentView(R.layout.main_activity);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment, new Accuiel())
                 .commit();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        toolbar.setNavigationOnClickListener(v -> dLayout.openDrawer(Gravity.LEFT));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dLayout.openDrawer(Gravity.LEFT);
+            }
+        });
         setNavigationDrawer();
 
     }
@@ -45,7 +46,11 @@ public class MainActivity extends AppCompatActivity {
     // à copier dans les autres mais inflate l'autre menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.nav_items, menu);
+        if (!userConnected) {
+            getMenuInflater().inflate(R.menu.nav_items, menu);
+            return true;
+        }
+        getMenuInflater().inflate(R.menu.connected_nav_item, menu);
         return true;
     }
 
@@ -53,20 +58,19 @@ public class MainActivity extends AppCompatActivity {
         dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navView = (NavigationView) findViewById(R.id.navigation);
         navView.setNavigationItemSelectedListener(item -> {
-            navView.setNavigationItemSelectedListener(menuItem -> {
-                Intent intent = null;
-                int itemId = menuItem.getItemId();
-                if (itemId == R.id.accueil_item) {
-                    intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }
-                Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-                if (intent != null) {
-                    dLayout.closeDrawers();
-                    return true;
-                }
-                return false;
-            });
+            Fragment fragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.accueil_item) {
+                fragment = new Accuiel();
+            }
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+            if (fragment != null) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment, fragment);
+                ft.commit();
+                dLayout.closeDrawers();
+                return true;
+            }
             return false;
         });
     }
