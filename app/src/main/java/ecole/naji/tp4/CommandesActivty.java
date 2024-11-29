58ac926b80f8a4e2f4d3465efbe76cc605e7db46
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-11-29 09:59:45 -0500

    jsp si sa marche mais on vera quadn tu va pull olo

--- app/src/main/java/ecole/naji/tp4/CommandesActivty.java
+++ app/src/main/java/ecole/naji/tp4/CommandesActivty.java
@@ -1,6 +1,7 @@
 package ecole.naji.tp4;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -14,10 +15,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.util.List;
+
 
 import ecole.naji.tp4.adaptaters.CoolAdapater;
 import ecole.naji.tp4.models.Pizza;
@@ -27,21 +42,29 @@ public class CommandesActivty extends Fragment {
     private ListView PIZZAZ;
     private CoolAdapater pizzaAdapter;
     private List<Pizza> pizzaList;
+
+    private DatabaseManger data;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.commande, container, false);
 
-
+        data = DatabaseManger.getInstance(getContext());
 
 
         PIZZAZ = rootView.findViewById(R.id.idkidkidkidkidk);
 
-        pizzaList = new ArrayList<>();
-        pizzaList.add(new Pizza("Pepperoni", "Spicy", 40, R.drawable.p1));
-        pizzaList.add(new Pizza("Vérégatien", "Viande", 300, R.drawable.guy));
-        pizzaList.add(new Pizza("Grosse", "30pouce", 5, R.drawable.p3));
-        pizzaList.add(new Pizza("Dick's cheese", "6", 10, R.drawable.p4));
-        pizzaList.add(new Pizza("Félix", "medium rare", 132, R.drawable.p5));
+        Log.i("runtime", String.valueOf(data));
+
+
+        pizzaList = data.readPizzasses();
+        // SI PAS PIZZA AJOUTER PIZZA DÉFAUT
+        if (pizzaList.size() < 3) {
+            data.insertPizze(new Pizza("Pepperoni", "Spicy", 40, R.drawable.p1));
+            data.insertPizze(new Pizza("Vérégatien", "Viande", 300, R.drawable.guy));
+            data.insertPizze(new Pizza("Grosse", "30pouce", 5, R.drawable.p3));
+            data.insertPizze(new Pizza("Dick's cheese", "6", 10, R.drawable.p4));
+            data.insertPizze(new Pizza("Félix", "medium", 132, R.drawable.p5));
+        }
 
         pizzaAdapter = new CoolAdapater(pizzaList, getContext());
         PIZZAZ.setAdapter(pizzaAdapter);
--- app/src/main/java/ecole/naji/tp4/models/Commande.java
+++ app/src/main/java/ecole/naji/tp4/models/Commande.java
@@ -7,18 +7,15 @@ import com.j256.ormlite.table.DatabaseTable;
 public class Commande {
     @DatabaseField(columnName = "id", generatedId = true)
     private int id;
-    @DatabaseField(columnName = "no_commande")
-    private int noCommande;
     @DatabaseField(columnName = "nom_client")
-    private String nomClient;
+    private int idClient;
     @DatabaseField(columnName = "montant")
     private double montant;
     @DatabaseField(columnName = "date")
     private String date;
 
-    public Commande(int noCommande, String nomClient, double montant, String date) {
-        this.noCommande = noCommande;
-        this.nomClient = nomClient;
+    public Commande(int idClient, double montant, String date) {
+        this.idClient = idClient;
         this.montant = montant;
         this.date = date;
     }
@@ -26,20 +23,12 @@ public class Commande {
     public Commande() {
     }
 
-    public int getNoCommande() {
-        return noCommande;
+    public int getIdClient() {
+        return idClient;
     }
 
-    public void setNoCommande(int noCommande) {
-        this.noCommande = noCommande;
-    }
-
-    public String getNomClient() {
-        return nomClient;
-    }
-
-    public void setNomClient(String nomClient) {
-        this.nomClient = nomClient;
+    public void setIdClient(int idClient) {
+        this.idClient = idClient;
     }
 
     public double getMontant() {
@@ -61,8 +50,8 @@ public class Commande {
     @Override
     public String toString() {
         return "Commande{" +
-                "noCommande=" + noCommande +
-                ", nomClient='" + nomClient + '\'' +
+                "noCommande=" + id +
+                ", nomClient='" + idClient + '\'' +
                 ", montant=" + montant +
                 ", date='" + date + '\'' +
                 '}';
