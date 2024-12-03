4fabe9bea8b0b43eb891f8bdb474c7ba2e687c8d
Felix Blanchette <felix.blanchette@gmail.com>
2024-12-03 13:48:57 -0500

    lcb c'est de ta faute si ca chie!

--- app/src/main/java/ecole/naji/tp4/adaptaters/CommandesAdapter.java
+++ app/src/main/java/ecole/naji/tp4/adaptaters/CommandesAdapter.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,11 +26,16 @@ public class CommandesAdapter extends BaseAdapter {
     private List<Commande> commandeList;
     private Context context;
     private DatabaseManger db;
+    TextView price;
+    TextView total;
+    TextView qte;
+    private Runnable yeah;
 
-    public CommandesAdapter(Context context, DatabaseManger db, List<Commande> comms) {
+    public CommandesAdapter(Context context, DatabaseManger db, List<Commande> comms, Runnable yeah) {
         this.db = db;
         this.commandeList = comms.stream().filter(c -> c.getIdClient() == MainActivity.userConnected).collect(Collectors.toList());
         this.context = context;
+        this.yeah = yeah;
     }
 
     @Override
@@ -52,9 +59,12 @@ public class CommandesAdapter extends BaseAdapter {
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.cool_commandes_model_pour_les_trucs_cool, parent, false);
         }
+
+        Commande ocm    = commandeList.get(position);
+
         Pizza pidz = db.readPizzasses().stream()
                 .filter(p -> {
-                    boolean match = p.getId() == commandeList.get(position).getPidzId();
+                    boolean match = p.getId() == ocm.getPidzId();
                     Log.i("PizzaMatch", String.valueOf(p.getId()));
                     Log.i("PizzaMatch", String.valueOf(commandeList.get(position)));
                     Log.i("PizzaMatch", String.valueOf(commandeList.get(position).getPidzId()));
@@ -69,30 +79,38 @@ public class CommandesAdapter extends BaseAdapter {
                 .orElseThrow(() -> new IllegalArgumentException("Pizza not found for the given ID"));
 
         ImageView pidzImg = convertView.findViewById(R.id.imgPidz);
-        TextView price = convertView.findViewById(R.id.price);
-        TextView total = convertView.findViewById(R.id.total_price);
+        total = convertView.findViewById(R.id.total_price);
         Button add = convertView.findViewById(R.id.plus);
         Button sub = convertView.findViewById(R.id.moins);
-        TextView qte = convertView.findViewById(R.id.qte);
+        qte = convertView.findViewById(R.id.qte);
+        price = convertView.findViewById(R.id.price);
         pidzImg.setImageResource(pidz.getImgId());
-        price.setText(pidz.getPrix() + "");
         qte.setText("1");
+        price.setText(pidz.getPrix() + "");
         total.setText((pidz.getPrix() * Double.parseDouble((String) qte.getText())) + "");
-        add.setOnClickListener(e -> qte.setText(handleAdd((String) qte.getText())));
-        sub.setOnClickListener(e -> qte.setText(handleSub((String) qte.getText())));
+        add.setOnClickListener(e -> qte.setText(handleAdd((String)qte.getText(), pidz, ocm)));
+        sub.setOnClickListener(e -> qte.setText(handleSub((String)qte.getText(), pidz, ocm)));
 
         return convertView;
     }
 
-    private String handleSub(String text) {
+    private String handleSub(String text, Pizza pidz, Commande command){
         Log.i("asd", " --- ");
         double value = Double.parseDouble(text);
+        total.setText(pidz.getPrix() * (value - 1) + "");
+        command.setMontant(pidz.getPrix() * (value - 1));
+        db.updateCommmandeClien(command);
+        yeah.run();
         return (value - 1) + "";
     }
 
-    private String handleAdd(String text) {
+    private String handleAdd(String text, Pizza pidz, Commande command) {
         Log.i("asd", " +++ ");
         double value = Double.parseDouble(text);
+        total.setText(pidz.getPrix() * (value + 1) + "");
+        command.setMontant(pidz.getPrix() * (value + 1));
+        db.updateCommmandeClien(command);
+        yeah.run();
         return (value + 1) + "";
     }
 }
